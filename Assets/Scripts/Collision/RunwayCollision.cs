457da6c6eecbd4ae71963e56ebba503e9b7337ae
KamilSzczepankiewicz <64781776+KamilSzczepankiewicz@users.noreply.github.com>
2022-05-26 10:54:24 +0100

    tag system for runways
    List of strings allows to have different type of runways accepting various types of aero-vehicles <--

--- Assets/Scripts/Collision/RunwayCollision.cs
+++ Assets/Scripts/Collision/RunwayCollision.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class RunwayCollision : MonoBehaviour
 {
+    public List<string> allowedOnRunway = new List<string>();
     public void OnTriggerEnter2D(Collider2D other)
     {
         ILandable IPlane = other.gameObject.GetComponent<MovementScript>() as ILandable;
         //Debug.Log($"Is null?  {IPlane == null}");
         if (IPlane != null)
         {
+            if(allowedOnRunway.Count == 0) return;
+            foreach (string tag in allowedOnRunway)
+            {
+                Debug.Log($"{tag} other is {other.gameObject.tag}");
+                if (other.gameObject.tag == tag)
+                {
+                    break;
+                }
+                return;
+            }
             if (IPlane.IsLanding())
             {
                 Destroy(other.gameObject);
