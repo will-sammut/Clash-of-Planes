f68e37054dec3ab7bb72589e7657bb1be3d763e0
Will Sammut <willsammut10@gmail.com>
2022-05-26 16:14:24 +0100

    Main Menu credits button and quit button functionality

--- Assets/Scripts/UI/PauseSystem.cs
+++ Assets/Scripts/UI/PauseSystem.cs
@@ -19,7 +19,7 @@ public class PauseSystem : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             gameManager.PauseToggle();
             UI.SetActive(gameManager.isPaused);
