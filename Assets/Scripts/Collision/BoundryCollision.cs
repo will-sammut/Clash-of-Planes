410982ef8775e4ac96406554d34d25320848dc5f
Jessica Christensen <65625958+C-Jess@users.noreply.github.com>
2022-05-26 15:15:39 +0100

    added spawn warning

--- Assets/Scripts/Collision/BoundryCollision.cs
+++ Assets/Scripts/Collision/BoundryCollision.cs
@@ -4,39 +4,113 @@ using UnityEngine;
 
 public class BoundryCollision : MonoBehaviour
 {
-    Camera cam;
-    public bool bounceable;
+    private Camera cam;
+    private bool onScreen;
+    [SerializeField] private GameObject warningObject;
+    private GameObject warning;
+    private bool warningOn;
+    [SerializeField] private float warningOffset;
+    [SerializeField] private float boundryWarningOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        bounceable = false;
+        onScreen = false;
         cam = Camera.main;
+        warningOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 screenPos = cam.WorldToScreenPoint(transform.position);
-        Debug.Log($"{Screen.height} {screenPos.y}, {Screen.width} {screenPos.x}");
-        if (bounceable)
+        if (onScreen)
         {
             if ((screenPos.y >= Screen.height) || (screenPos.y <= 0f))
             {
                 GetComponent<MovementScript>().BounceVertical();
-                bounceable = false;
+                onScreen = false;
             }
             if ((screenPos.x >= Screen.width) || (screenPos.x <= 0f))
             {
                 GetComponent<MovementScript>().BounceHorizontal();
-                bounceable = false;
+                onScreen = false;
             }
         }
         else
         {
+            // becomes on screen
             if ((screenPos.y < Screen.height) && (screenPos.y > 0f) && (screenPos.x < Screen.width) && (screenPos.x > 0f))
             {
-                bounceable = true;
+                onScreen = true;
+                Destroy(warning);
+                warningOn = false;
+            }
+
+            if (!warningOn)
+            {
+                AddWarning(screenPos);
+            }
+            else
+            {
+                UpdateWarning(screenPos);
             }
         }
+    }
+
+    private void AddWarning(Vector2 planePos)
+    {
+        // off to left
+        if (planePos.x < 0f - boundryWarningOffset)
+        {
+            warning = Instantiate(warningObject, Vector2ScreenToWorldPoint(new Vector2(warningOffset, planePos.y), 0), Quaternion.identity);
+            warningOn = true;
+        }
+        // off to right
+        if (planePos.x > Screen.width + boundryWarningOffset)
+        {
+            warning = Instantiate(warningObject, Vector2ScreenToWorldPoint(new Vector2(Screen.width - warningOffset, planePos.y), 0), Quaternion.identity);
+            warningOn = true;
+        }
+        // off to bottom
+        if (planePos.y < 0f - boundryWarningOffset)
+        {
+            warning = Instantiate(warningObject, Vector2ScreenToWorldPoint(new Vector2(planePos.x, warningOffset), 0), Quaternion.identity);
+            warningOn = true;
+        }
+        // off to top
+        if (planePos.y > Screen.height + boundryWarningOffset)
+        {
+            warning = Instantiate(warningObject, Vector2ScreenToWorldPoint(new Vector2(planePos.x, Screen.height - warningOffset), 0), Quaternion.identity);
+            warningOn = true;
+        }
+    }
+
+    private void UpdateWarning(Vector2 planePos)
+    {
+        if (planePos.x < 0f - boundryWarningOffset)
+        {
+            warning.GetComponent<Transform>().position = Vector2ScreenToWorldPoint(new Vector2(warningOffset, planePos.y), 0);
+        }
+        // off to right
+        if (planePos.x > Screen.width + boundryWarningOffset)
+        {
+            warning.GetComponent<Transform>().position = Vector2ScreenToWorldPoint(new Vector2(Screen.width - warningOffset, planePos.y), 0);
+        }
+        // off to bottom
+        if (planePos.y < 0f - boundryWarningOffset)
+        {
+            warning.GetComponent<Transform>().position = Vector2ScreenToWorldPoint(new Vector2(planePos.x, warningOffset), 0);
+        }
+        // off to top
+        if (planePos.y > Screen.height + boundryWarningOffset)
+        {
+            warning.GetComponent<Transform>().position = Vector2ScreenToWorldPoint(new Vector2(planePos.x, Screen.height - warningOffset), 0);
+        }
+    }
+
+    private Vector3 Vector2ScreenToWorldPoint(Vector2 point, float z)
+    {
+        return new Vector3(cam.ScreenToWorldPoint(point).x, cam.ScreenToWorldPoint(point).y, z);
     }
 }
