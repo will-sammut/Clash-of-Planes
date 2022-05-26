6d32246795f5e045fa98da6ec8cb50acfde462b3
KamilSzczepankiewicz <64781776+KamilSzczepankiewicz@users.noreply.github.com>
2022-05-26 14:10:30 +0100

    Particle explosion
    Planes go boom

--- Assets/Scripts/Collision/PlaneCollision.cs
+++ Assets/Scripts/Collision/PlaneCollision.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlaneCollision : MonoBehaviour
 {
+    public UnityEvent onCollision;
+    public GameObject particles;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (ObjectIsVisible.collidable)
         {
+            if (particles != null)
+            {
+                Instantiate(particles,transform.position,Quaternion.identity);
+            }
+            onCollision.Invoke();
             // End the game
             Destroy(transform.parent.gameObject);
-            Debug.Log($"{transform.parent.gameObject.name} has collided");
+            //Debug.Log($"{transform.parent.gameObject.name} has collided");
         }
     }
 }
